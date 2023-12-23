e63cca3e71e98eebc9c7ce574e1edad7cb938417
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-23 20:04:41 +0530

    Implement basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from 'react';
+import data from './data';
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  const createList = (person) => {
+    const { id, name, age, image } = person;
+    return (
+      <li style={{ margin: '15px' }}>
+        <img
+          src={image}
+          alt={name}
+          style={{
+            maxWidth: '300px',
+            maxHeight: '250px',
+            margin: '15px',
+            borderRadius: '8px',
+          }}
+        />
+        <h3>{name}</h3>
+        <h4>{age}</h4>
+      </li>
+    );
+  };
+
+  return (
+    <div>
+      <h2>Birthday Reminder</h2>
+      <h3>{people.length}</h3>
+      <ul>{data.map(createList)}</ul>
+    </div>
+  );
 };
 export default App;
